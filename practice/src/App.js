41d2459149e433e25fbb3177a590b66466b031f4
Srutip04 <subuhishruti75@gmail.com>
2022-12-22 19:52:33 +0530

    margin property

--- practice/src/App.js
+++ practice/src/App.js
@@ -4,7 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='font-mono font-bold text-3xl'>Hello World</h1>
+      <h1 className="font-mono font-bold text-3xl text-cyan-400">
+        Hello World
+      </h1>
+      <div className="w-36 h-12 bg-slate-500">Div</div>
+      {/* margin top */}
+      <div className="w-36 h-12 bg-slate-600 mt-6">mt-6</div>
+      {/* margin right */}
+      <div className="w-36 h-12 bg-slate-600 mr-4">mr-4</div>
+      {/* margin left */}
+      <div className="w-36 h-12 bg-slate-600 ml-2">ml-2</div>
+      {/* margin bottom */}
+      <div className="w-36 h-12 bg-slate-600 mb-8">mb-8</div>
     </div>
   );
 }
